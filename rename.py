2b8c478eacf16a1ab72622afe846d445c60cb395
zecy <angelszc163@gmail.com>
2017-02-15 22:04:53 +0800

    完成扁平化图片集

--- rename.py
+++ rename.py
@@ -2,9 +2,9 @@
 #! /Usr/local/bin/python3
 """
     本脚本用于对图片集进行重命名包括
-    1. 补零 @leftpad
-    2. 添加目录前缀 @dirprefix
-    3. 通过对译表重命名 @renamemap
+    1. 补零               @leftpad      DONE
+    2. 扁平化图片集       @flatten      DONE
+    3. 通过对译表重命名   @renamemap    TODO
 """
 
 import os
@@ -14,23 +14,47 @@ image = re.compile(r'(.*)\.(jpg|png)')
 
 
 def parseimgs(path):
+    """ 分析 path 中有多少图片，返回图片 list """
     files = next(os.walk(path))[2]
     images = [f for f in files if image.match(f)]
     return images
 
 
-def leftpad(arr):
-    length = len(str(len(arr)))  # (计算长度 (转为str (数组的长度，等同于最大的数)))
+def leftpad(src, length):
+    """ 根据 length 为 src 左方补零 """
+    name = image.match(src).group(1)
+    suffix = image.match(src).group(2)
+    return name.rjust(length, '0') + '.' + suffix
+
+
+def padnum(arr):
+    """ 计算需补零的个数
+        (计算长度 (转为str (数组的长度，等同于最大的数)))"""
+    return len(str(len(arr)))
+
+
+def leftpadall(arr):
+    """ 把 arr 中所有文件补零 """
     for src in arr:
-        name = image.match(src).group(1)
-        suffix = image.match(src).group(2)
-        dst = name.rjust(length, '0') + '.' + suffix
+        dst = leftpad(src, padnum(arr))
         os.replace(src, dst)
 
 
+def flatten(path):
+    """ 分析 path 下有多少个子目录，
+        为子目录中所有图片补零，并添加上文件夹名前缀，
+        提出到父目录 """
+    dirs = next(os.walk(path))[1]
+    for folder in dirs:
+        imgs = parseimgs(folder)
+        for img in imgs:
+            dst = folder + '-' + leftpad(img, padnum(imgs))
+            os.replace(folder + '/' + img, dst)
+
+
 def main(path):
-    imgs = parseimgs(path)
-    leftpad(imgs)
+    """ 默认运行的主函数 """
+    flatten(path)
 
 if __name__ == '__main__':
     main('.')
