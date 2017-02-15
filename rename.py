9c7fcda887603943da5ebed23b8f3670791722aa
zecy <angelszc163@gmail.com>
2017-02-15 21:19:29 +0800

    完成补零功能

--- rename.py
+++ rename.py
@@ -2,24 +2,35 @@
 #! /Usr/local/bin/python3
 """
     本脚本用于对图片集进行重命名包括
-    1. 统一编号 @numberic
+    1. 补零 @leftpad
     2. 添加目录前缀 @dirprefix
     3. 通过对译表重命名 @renamemap
 """
 
 import os
-import sys
 import re
 
-suffix = re.compile('.*\.(jpg|png)')
+image = re.compile(r'(.*)\.(jpg|png)')
+
 
 def parseimgs(path):
-    files  = next(os.walk(path))[2]
-    images = [f for f in files if suffix.match(f)]
+    files = next(os.walk(path))[2]
+    images = [f for f in files if image.match(f)]
     return images
 
+
+def leftpad(arr):
+    length = len(str(len(arr)))  # (计算长度 (转为str (数组的长度，等同于最大的数)))
+    for src in arr:
+        name = image.match(src).group(1)
+        suffix = image.match(src).group(2)
+        dst = name.rjust(length, '0') + '.' + suffix
+        os.replace(src, dst)
+
+
 def main(path):
-    print(parseimgs(path))
+    imgs = parseimgs(path)
+    leftpad(imgs)
 
 if __name__ == '__main__':
     main('.')
