67109a9433f0afb7399b2969138410c93c4fd0c5
zecy <angelszc163@gmail.com>
2017-02-18 17:23:52 +0800

    main.py 补充了文档，并更名为 imgafter.py

--- imgafter.py
+++ imgafter.py
@@ -1,5 +1,26 @@
-# _*_ coding: utf-8 -*-
-""" 入口函数 """
+# -*- coding: utf-8 -*-
+"""
+用法：imgafter <命令名> [-m <对译表名称>] [-d <目标路径>]
+
+      [命令]
+
+      leftpad 对当前目录下的图片文件名补零。
+
+      flatten 为当前目录下子文件夹中图片添加子文件夹名称为前缀，
+              并提到当前目录。
+
+      map     依据对译表对当前目录下的图片文件进行重命名。
+
+      zip     打包当前目录下所有图片，并把压缩包移到上一级目录。
+              压缩包名称为目录名称。
+
+      [参数]
+
+      -m : 文件名对译表，默认为 `map` ，一个无后缀名的纯文本文件
+      -d : 目标文件夹，默认为当前名录
+
+     默认情况下不需要设置上面两个参数
+"""
 
 import sys
 import getopt
@@ -8,33 +29,67 @@ import zipimgs
 
 
 def script_name():
-    return sys.argv[0].split('/')[-1]  # 本脚本的文件名
+    """
+    本脚本的文件名
+
+    return: str
+
+    返回本脚本的名字，方便调用。
+    """
+    return sys.argv[0].split('/')[-1]
 
 
 def working_dir(path='.'):
-    return path  # 脚本工作路径，默认为调用本脚本的文件夹
+    """
+    脚本工作路径
+
+    return: str
+
+    返回本脚本的工作路径，默认为调用本脚本的文件夹。
+    """
+
+    return path
 
 
 def error_message():
+    """
+    错误信息
+
+    return: str
+
+    返回错误信息文本，方便调用。
+    """
+
     return "参数错误，通过`" + script_name() + " -h` 获取帮助"
 
 
 def help_message():
-    return ("\n用法：" + script_name() + " <命令名> [-m <对译表名称>] [-d <目标路径>]\n\n" +
-            "      <命令>\n\n" +
-            "      leftpad ：对当前目录下的图片文件名补零。\n\n" +
-            "      flatten ：为当前目录下子文件夹中图片添加子文件夹名称为前缀，\n" +
-            "                并提到当前目录。\n\n" +
-            "      map     ：依据对译表对当前目录下的图片文件进行重命名。\n\n" +
-            "      zip     ：打包当前目录下所有图片，并把压缩包移到上一级目录。\n" +
-            "                压缩包名称为目录名称。"
-            "      <参数>\n\n" +
-            "      -m : 文件名对译表，默认为 `map` ，一个无后缀名的纯文本文件\n" +
-            "      -d : 目标文件夹，默认为当前名录\n\n" +
-            "      默认情况下不需要设置上面两个参数")
+    """
+    帮助信息
+
+    return: str
+
+    返回帮助信息文本，方便调用。内容为本脚本的说明文档。
+    """
+
+    return __doc__
 
 
 def command_getter(argv):
+    """
+    获取命令
+
+    reutrn: dict
+        {
+            'command_name': command_name,
+            'command_mapname': command_mapname,
+            'command_dir': command_dir
+        }
+
+    ``command_name``      type: str  命令的名称，对应的命令通过 -h 查看。
+    ``command_mapname``   type: str  对译表名称。
+    ``command_dir``       type: str  执行命令的目录。
+    """
     opt_status = len(argv)
 
     command_name = ''
@@ -70,6 +125,13 @@ def command_getter(argv):
 
 
 def command_switcher(command_bundle):
+    """
+    命令切换器
+
+    根据 ``command_bundle`` 执行各个命令。命令用途通过 -h 查看。
+
+    ``command_bundle``  ``command_getter``的返回值。
+    """
     command_name = command_bundle['command_name']
     command_mapname = command_bundle['command_mapname']
     command_dir = command_bundle['command_dir']
@@ -99,8 +161,6 @@ def main(argv):
         exe_command(argv)
     except getopt.GetoptError:
         print(error_message())
-
-    # rename.renamemap(path, mapname)
 
 if __name__ == '__main__':
     main(sys.argv)
