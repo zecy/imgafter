d985ecac9e7187261c4017148863c9aef8267357
zecy <angelszc163@gmail.com>
2017-02-18 17:27:01 +0800

    用于把目录下所有纯图片子文件夹打包

--- zipimgs.py
+++ zipimgs.py
@@ -32,5 +32,44 @@ def zip_cur_imgs():
         print("压缩文件错误：\n    ", err)
 
 
+def zip_sub_dir_imgs(path="."):
+    """ 把目录下的子目录各自进行打包，压缩包名称为目录名称。"""
+
+    sub_dirs = next(os.walk(path))[1]
+
+    img_dirs = []
+    no_img_dirs = []
+    dir_already_zip = []
+    zip_count = 0
+
+    for sub_dir in sub_dirs:
+        if basefunc.all_imgs(sub_dir):
+            img_dirs.append(sub_dir)
+        else:
+            no_img_dirs.append(sub_dir)
+
+    for img_dir in img_dirs:
+        try:
+            if not os.path.exists(img_dir + ".zip"):
+                os.chdir(img_dir)
+                zip_cur_imgs()
+                os.chdir("..")
+                zip_count += 1
+            else:
+                dir_already_zip.append(img_dir + " ==> " + img_dir + ".zip")
+        except FileNotFoundError:
+            print("找不到目录：\n    ", img_dir)
+
+    print("\n[压缩结果]\n\n" +
+          "    共 " + str(len(sub_dirs)) + " 个文件夹，成功压缩 " + str(zip_count) + " 个文件夹。\n\n" +
+          "[未处理文件夹]\n\n" +
+          "    本命令只支持纯图片文件夹，因此以下文件夹未被压缩，如果它们是目标文件夹，请检查：\n\n" +
+          "        " + "\n        ".join(no_img_dirs) + "\n\n" +
+          "    纯图片文件夹条件：\n\n" +
+          "        1.不为空；2.文件全部为图片；3.不包含子文件夹。\n\n" +
+          "[忽略文件夹]\n\n"
+          "    以下文件夹已经被压缩，因此未被处理。如果你希望重新压缩，请先删除原同名压缩包：\n\n" +
+          "        " + "\n        ".join(dir_already_zip))
+
 if __name__ == '__main__':
     main()
